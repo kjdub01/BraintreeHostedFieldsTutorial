06ed73317b9c29c9f303cdf20da6f687634cfc33
Kate Jewett-Williams <36312087+kjdub01@users.noreply.github.com>
2024-12-04 20:19:00 +0000

    add checkout route to router.

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// The checkout route
+const checkout = require("./routes/checkout");
+app.use("/checkout", checkout);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
